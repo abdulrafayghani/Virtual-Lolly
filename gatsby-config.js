ee449bd3ba859447367899a95075bc2fc16b23b6
abdulrafayghani <abdulrafay.ghani@gmail.com>
2020-11-04 16:09:47 +0500

    netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,26 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-const { createHttpLink } = require('apollo-link-http')
-const fetch = require('node-fetch')
- require('dotenv').config()
+// /**
+//  * Configure your Gatsby site with this file.
+//  *
+//  * See: https://www.gatsbyjs.com/docs/gatsby-config/
+//  */
+// const { createHttpLink } = require('apollo-link-http')
+// const fetch = require('node-fetch')
+//  require('dotenv').config()
 
 
-module.exports = {
-  /* Your site config here */
-  plugins: [
-    {
-      resolve: "gatsby-source-graphql",
-      options: {
-        typeName: "Lolly",
-        fieldName: "lolly",
-        // url: `http://localhost:8888/.netlify/functions/lolly`,
-        createLink: () =>  {
-          createHttpLink({ uri: `/.netlify/functions/lolly`, fetch })
-        }
-      },
-    },
-  ],
-}
+// module.exports = {
+//   /* Your site config here */
+//   plugins: [
+//     {
+//       resolve: "gatsby-source-graphql",
+//       options: {
+//         typeName: "Lolly",
+//         fieldName: "lolly",
+//         // url: `http://localhost:8888/.netlify/functions/lolly`,
+//         createLink: () =>  {
+//           createHttpLink({ uri: `/.netlify/functions/lolly`, fetch })
+//         }
+//       },
+//     },
+//   ],
+// }
